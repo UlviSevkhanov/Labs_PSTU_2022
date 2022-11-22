acc65eb01b0751e35fcd29bac633082c466636f9
UlviSevkhanov <ulvisevxanov@mail.ru>
2022-11-22 18:39:48 +0500

    Update 2-22.cpp

--- Algorithms/2-22.cpp
+++ Algorithms/2-22.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	float n, a, pos=0, max = -1;
+	float n, a, pos=0, max,i=1;
 
 	cout << "Введите количество элементов n" << endl;
 	cin >> n;
 
-	for (int i = 1; i <= n; i++)
+	max = sin(n + (i / n));
+
+	for (int i = 2; i <= n; i++)
 	{
 		a = sin(n + (i / n));
 		if (a > max)
