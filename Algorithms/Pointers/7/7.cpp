0aea80c812697de63a4ddf7f6fb525c66f059654
UlviSevkhanov <ulvisevxanov@mail.ru>
2022-12-12 21:58:54 +0500

    Update 7.cpp

--- Algorithms/Pointers/7/7.cpp
+++ Algorithms/Pointers/7/7.cpp
@@ -4,11 +4,17 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	int n;
+	int n_new;
 	int* ptr = &n;
 
-	std::cout << "Введите число " << std::endl;
+	std::cout << "Введите значение переменной n" << std::endl;
 	std::cin >> *ptr;
 
+	std::cout << "Введите новое значение перменной" << std::endl;
+	std::cin >> n_new;
+
+	*ptr = n_new;
+	
 	std::cout << "Изменное значение переменной равно " << n << std::endl;
 
 	return 0;
