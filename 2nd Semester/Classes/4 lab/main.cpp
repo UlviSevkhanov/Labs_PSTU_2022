442abac5d3bfd098fded45e60a8c4442163f0649
UlviSevkhanov <ulvisevxanov@mail.ru>
2023-04-08 15:06:15 +0500

    4 lab classes

--- 2nd Semester/Classes/4 lab/main.cpp
+++ 2nd Semester/Classes/4 lab/main.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
-#include "Pair.h"
-#include "Long.h"
+#include "Person.h"
+#include "Student.h"
+#include <string>
 
-int main()
+void f1(Person& f)
 {
-	setlocale(LC_ALL, "RUS");
-	Pair A;
-	Pair B;
-	std::cin >> A >> B;
-	std::cout << "Pair A = " << A << std::endl;
-	std::cout << "Pair B = " << B << std::endl;
-	std::cout << "A + B = " << A + B << std::endl;
-	Long a(A.getFirst(A), A.getSecond(A)), b(B.getFirst(B), B.getSecond(B));
-	std::cout << "Long a = " << a << std::endl;
-	std::cout << "Long b = " << b << std::endl;
-	std::cout << "a + b = " << a + b << std::endl;
-	std::cout << "a - b = " << a - b << std::endl;
-	std::cout << "a * b = " << a * b << std::endl;
+	f.setName("Вова");
+	std::cout << f << "\n";
+}
+
+Person f2()
+{
+	Student s("Олег", 20, 3);
+	return s;
+}
+
+int  main()
+{
+	setlocale(LC_ALL, "rus");
+	Person human1("Петя", 19);
+	std::cout << human1;
+	std::cout << std::endl;
+
+	Student human2("Иван", 18, 1);
+	std::cout << human2;
+	std::cout << "Год с увеличением: " << human2.increase_year_of_study() << "\n";
+
+	std::cout << std::endl;
+	Student human3 = human2;
+	std::cout << human3;
+	std::cout << std::endl;
+	Student human4;
+	f1(human4);
+	human1 = f2();
+	std::cout << human1;
 	return 0;
 }
+
